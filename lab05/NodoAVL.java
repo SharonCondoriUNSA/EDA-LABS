89b016410365abeccf121b8fc802d6bd1caa79c4
Joselin Sharon Condori Catunta <jcondoricat@unsa.edu.pe>
2025-06-18 01:51:40 -0500

    Subiendo datos de nodoavl

---
+++ lab05/NodoAVL.java
@@ -0,0 +1,11 @@
+class NodoAVL {
+    int valor;
+    int altura;
+    NodoAVL izquierdo;
+    NodoAVL derecho;
+
+    NodoAVL(int valor) {
+        this.valor = valor;
+        this.altura = 1;
+    }
+}
