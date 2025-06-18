db36ac18eb6f5e98c75b09e6e6cf8b6cc585abf9
Joselin Sharon Condori Catunta <jcondoricat@unsa.edu.pe>
2025-06-18 02:17:22 -0500

    Main.java modificación de salida

--- lab05/Main.java
+++ lab05/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String palabra = "HOLA";
+        String palabra = "UNSA";
         ArbolAVL arbol = new ArbolAVL();
 
         NodoAVL raiz = null;
@@ -12,17 +12,17 @@ public class Main {
             raiz = arbol.insert(raiz, ascii);
         }
 
-        System.out.println("\n- Búsqueda de 'L' (76): " + arbol.search(raiz, 76));
+        System.out.println("\n- Búsqueda de 'S' (83): " + arbol.search(raiz, 83));
         System.out.println("- Mínimo: " + arbol.getMin(raiz));
         System.out.println("- Máximo: " + arbol.getMax(raiz));
 
-        NodoAVL padre = arbol.parent(raiz, 79, null);
+        NodoAVL padre = arbol.parent(raiz, 78, null);
         if (padre != null)
-            System.out.println("- Padre de 79: " + padre.valor);
+            System.out.println("- Padre de 78: " + padre.valor);
         else
-            System.out.println("- Padre de 79: No encontrado");
+            System.out.println("- Padre de 78: No encontrado");
 
-        System.out.println("- Hijos de 72:");
-        arbol.son(raiz, 72);
+        System.out.println("- Hijos de 85:");
+        arbol.son(raiz, 85);
     }
 }
